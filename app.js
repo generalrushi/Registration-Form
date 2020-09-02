18505a30d6c795330842219d4ad561526647f653
Rushikesh Vyavahare <68723846+generalrushi@users.noreply.github.com>
2020-09-02 21:36:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const mobileno = document.getElementById("mobileno");
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+    const usernameValue = username.value.trim();
+    const mobilenoValue = mobileno.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+    
+    if(mobilenoValue === '') {
+		setErrorFor(mobileno, 'Mobile Number cannot be blank');
+    } else if (!isMobile(mobilenoValue)) {
+		setErrorFor(mobileno, 'Mobile Number must be in 10 digits');
+    }else {
+		setSuccessFor(mobileno);
+	} 
+    
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function isMobile(mobileno) {
+    return /^[0-9]{10}$/.test(mobileno);
+}
